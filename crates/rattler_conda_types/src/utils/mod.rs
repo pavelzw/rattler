7249e908d4000b6052e956079a783ce8a1fc18ea
Pavel Zwerschke <pavelzw@gmail.com>
2024-12-19 15:51:30 +0100

    Make UrlWithTrailingSlash public

--- crates/rattler_conda_types/src/utils/mod.rs
+++ crates/rattler_conda_types/src/utils/mod.rs
@@ -3,4 +3,4 @@ pub(crate) mod serde;
 pub(crate) mod url;
 pub(crate) mod url_with_trailing_slash;
 
-pub(crate) use url_with_trailing_slash::UrlWithTrailingSlash;
+pub use url_with_trailing_slash::UrlWithTrailingSlash;
